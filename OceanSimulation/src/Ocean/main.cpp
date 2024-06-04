5aa39bc6be9d6403c16efeb7374e348d07c098ae
aa66246624aa@gmail.com <aa66246624aa@gmail.com>
2024-06-05 03:26:54 +0800

    update

--- OceanSimulation/src/Ocean/main.cpp
+++ OceanSimulation/src/Ocean/main.cpp
@@ -219,7 +219,7 @@ void OceanContext::Display() {
     ocean.geometryType(geometryType);
     ocean.colors((float*)&fogColor, (float*)&emissiveColor,
         (float*)&ambientColor, (float*)&diffuseColor, (float*)&specularColor);
-    ocean.render(elapsedTime, lightPosition, projection, view, model, true);
+    ocean.render(elapsedTime, lightPosition, projection, view, model, false);
 
 #ifndef USE_OPENGL2_0
     // Finish using framebuffer
